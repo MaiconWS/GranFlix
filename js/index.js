25072f433fa3e49d38d0c561b11f1d7998f5d7c1
MaiconWS <maicon.w312@outlook.com>
2025-03-20 21:59:32 -0300

    Perfis

--- js/index.js
+++ js/index.js
@@ -11,3 +11,38 @@ document.addEventListener("DOMContentLoaded", async () => {
                 </a>`;
     }
 })
+
+const containerTrendingMovies = document.getElementById("trendingMovies");
+
+let scrollIntervalTrendingMovies;
+let scrollDirectionTrendingMovies = 0;
+
+containerTrendingMovies.addEventListener("mousemove", (e) => {
+    const boundingRect = containerTrendingMovies.getBoundingClientRect();
+    const mouseX = e.clientX;
+
+    const threshold = 200;
+
+    if (mouseX < boundingRect.left + threshold) {
+        scrollDirectionTrendingMovies = -1;
+        containerTrendingMovies.style.cusor = "url('/img/arrow-left.png'),auto";
+    } else if (mouseX > boundingRect.right - threshold){
+        scrollDirectionTrendingMovies = 1;
+    } else {
+        scrollDirectionTrendingMovies = 0;
+        containerTrendingMovies.style.cursor = "pointer";
+    }
+});
+
+containerTrendingMovies.addEventListener("mouseleave", () => {
+    scrollDirectionTrendingMovies = 0;
+    containerTrendingMovies.style.cursor = "default";
+});
+
+function autoScrollTrendingMovies() {
+    if(scrollDirectionTrendingMovies !== 0) {
+        containerTrendingMovies.scrollLeft += scrollDirectionTrendingMovies * 6;
+    }
+}
+
+scrollIntervalTrendingMovies =setInterval(autoScrollTrendingMovies,16);
