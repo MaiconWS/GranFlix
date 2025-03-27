663ac38890dad7490e0428c998995fd276223a28
MaiconMW <maicon.w312@outlook.com>
2025-03-27 00:39:45 -0300

    Conexão com API Themoviedb.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", async () => {
+    await banner();
+    await trendingMovies();
+    await toRateMovies();
+    toggleLoading();
+
     setTimeout(() => {
         toggleLoading();
     }, 2000);
@@ -10,7 +15,74 @@ document.addEventListener("DOMContentLoaded", async () => {
                     <img src="img/posters/${i}.jpg" alt="${i}">
                 </a>`;
     }
-})
+});
+
+
+async function banner() {
+    let trendings =[];
+    await fetch('https://api.themoviedb.org/3/trending/all/week?language=pt-BR', options)
+    .then(res => res.json())
+    .then(res => res.results.forEach(movie => {
+        trendings.push(movie);
+    }))
+    .catch(err => console.error(err));
+    let carousel = document.querySelector('.carousel-inner');
+    carousel.innerHTML = '';
+    for (let i = 0; i < trendings.length; i++) {
+        let active = i == 0 ? 'active' : '';
+        carousel.innerHTML += 
+            `<div class="carousel-item ${active}" data-bs-interval="10000">
+                <a href='detalhes.html?id=${trendings[i].id}&media=${trendings[i].media_type}'>
+                    <img src="https://image.tmdb.org/t/p/original/${trendings[i].backdrop_path}" class="d-block w-100" alt="">
+                    <div class="carousel-caption d-md-block">
+                        <h3>${trendings[i].title ?? trendings[i].name}</h3>
+                        <p class="d-none d-md-block">${trendings[i].overview}</p>
+                    </div>
+                </a>
+            </div>`;
+    }
+}
+
+async function trendingMovies() {
+    let trendings = [];
+    await fetch('https://api.themoviedb.org/3/trending/movie/day?language=pt-BR', options)
+        .then(res => res.json())
+        .then(res => res.results.forEach(movie => {
+            trendings.push(movie);
+        }))
+        .catch(err => console.error(err));
+
+    let trendingContainer = document.querySelector('#trendingMovies');
+    trendingContainer.innerHTML = '';
+    for (let i = 0; i < trendings.length; i++) {
+        trendingContainer.innerHTML +=
+            `<a href='detalhes.html?id=${trendings[i].id}&media=${trendings[i].media_type}'>
+                <img src="https://image.tmdb.org/t/p/original/${trendings[i].poster_path}" alt="">
+            </a>`;
+    }
+}
+
+async function topRatedMovies() {
+    let toprated = [];
+    await fetch('https://api.themoviedb.org/3/movie/top_rated?language=pt-BR', options)
+        .then(res => res.json())
+        .then(res => res.results.forEach(movie => {
+            toprated.push(movie);
+        }))
+        .catch(err => console.error(err));
+
+    //console.log(toprated);
+
+    let topRatedContainer = document.querySelector('#topRatedMovies');
+    topRatedContainer.innerHTML = '';
+    for (let i = 0; i < toprated.length; i++) {
+        topRatedContainer.innerHTML +=
+            `<a href='detalhes.html?id=${toprated[i].id}&media=movie'>
+                <img src="https://image.tmdb.org/t/p/original/${toprated[i].poster_path}" alt="">
+            </a>`;
+    }
+}
+
 
 const containerTrendingMovies = document.getElementById("trendingMovies");
 
--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNjZhZDE2MzYyMTJkZGFhY2ZmNzNjZjU4NGQ4NTM1NCIsIm5iZiI6MTc0MjUxNzA1Ni43ODksInN1YiI6IjY3ZGNiMzQwZDY4MTA0NTI3OTY5NmI4NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GzAs4bTNIf_Z-1uYfH_93BUt_iKl96320jDSQpKjUjQ'
+    }
+}
+
+
+
 function toggleLoading() {
     let loader = document.querySelector(".loader");
     loader.style.display = loader.style.display == "none" ? "block" : "none";
